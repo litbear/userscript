8578e585b68e8afc93074c095df27bc4b3d34028
litbear <litbear@foxmail.com>
2016-01-04 15:17:31 +0800

    增加 tistory网站优化

---
+++ tistory网站优化/tistory网站优化.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        tistory网站优化
+// @namespace   https://github.com/litbear
+// @description tistory 启用邮件，查看高质量图片
+// @include     http://*.tistory.com/*
+// @include     http://cfile\d+.uf.tistory.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+(function () {
+    //console.log('initialize');
+    // 重新启用右键菜单和鼠标选择
+    contextMenu();
+    // 优化图片大小
+    if(/http:\/\/cfile\d+.uf.tistory.com\/.*/.test(location.href)){
+        zoomPlus();
+    }
+    //console.log('finish');
+})();
+
+function contextMenu(){
+    document.oncontextmenu = new Function ('return true');
+    document.ondragstart = new Function ('return true');
+    document.onselectstart = new Function ('return true');
+    document.body.style.MozUserSelect = '-moz-all';
+}
+
+function zoomPlus(){
+    //console.log('preg');
+    
+    var regex = /image/;
+
+    if (regex.test(location.href))
+        location.href = location.href.replace(regex, "original");
+}
